15583909153d0a23f656347110ca27d7ced58075
shsep <iwtghome@gmail.com>
2025-07-21 17:22:38 +0900

    Initial commit

---
+++ src/main/java/com/shs/playrabbitmqbackend/PlayRabbitmqBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shs.playrabbitmqbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayRabbitmqBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayRabbitmqBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shs/playrabbitmqbackend/PlayRabbitmqBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shs.playrabbitmqbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayRabbitmqBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
